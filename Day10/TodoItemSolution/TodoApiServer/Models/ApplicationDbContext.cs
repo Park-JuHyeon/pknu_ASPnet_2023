ee64b527cad7930f4dabddca3ccafa19686193e7
Park-JuHyeon <j-hyeon97@naver.com>
2023-05-31 12:26:40 +0900

    0531 RESTApi 커밋

---
+++ Day10/TodoItemSolution/TodoApiServer/Models/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TodoApiServer.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        // 없을시 DB와 연결이 안됨
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
